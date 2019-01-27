56a6cf8b4659072590e4b9fd80c9cbac3a0edd00
turtleman270 <ahill5067@gmail.com>
2019-01-27 09:00:09 -0500

    removed old files

--- Assets/Scripts/clickDetector.cs
+++ Assets/Scripts/clickDetector.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class clickDetector : MonoBehaviour {
 
-	public MovementTracker tracker; 
 	public GameObject prefabBug;
 	public GameObject startPosition;
 	public float startTime;
