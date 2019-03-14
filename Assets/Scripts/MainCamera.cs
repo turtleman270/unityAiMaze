e772e7ee0a5d7d950614916ed7cf321075dc93a1
turtleman270 <ahill5067@gmail.com>
2019-03-13 21:12:24 -0400

    use normal buttons

---
+++ Assets/Scripts/MainCamera.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour {
+	public GameObject camera;
+	// Use this for initialization
+	void Start () {
+		camera.transform.position = configs.cameraPos;
+		camera.transform.rotation = configs.cameraRot;
+	}
+}
---
+++ Assets/Scripts/MazeInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeInfo : MonoBehaviour {
+
+	public string mazeName{ get; set; }
+
+	public Vector3 robotStartPos { get; set; }
+	public Quaternion robotStartRot { get; set; }
+
+	public Vector3 cameraPos { get; set; }
+	public Quaternion cameraRot { get; set; }
+
+	public MazeInfo(string mazeName, Vector3 robotStartPos, Quaternion robotStartRot,
+		Vector3 cameraPos, Quaternion cameraRot){
+		this.mazeName = mazeName;
+		this.robotStartPos = robotStartPos;
+		this.robotStartRot = robotStartRot;
+		this.cameraPos = cameraPos;
+		this.cameraRot = cameraRot;
+	}
+}
